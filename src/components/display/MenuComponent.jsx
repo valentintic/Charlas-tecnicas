51834e66acbfc80ff73ff1ee59fad6ee17772c76
valentin preutesei <valentinpreuteseitic@gmail.com>
2024-12-12 19:15:23 +0100

    Cambios charlas

--- src/components/display/MenuComponent.jsx
+++ src/components/display/MenuComponent.jsx
@@ -1,48 +1,92 @@
-import React, { Component } from 'react'
-import { NavLink } from 'react-router-dom'
-
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+import styles from "./Navbar.module.css"; 
+import { FaHome } from "react-icons/fa";
 
 export default class MenuComponent extends Component {
+  state = {
+    isAuthenticated: localStorage.getItem("token") !== null
+  };
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      const currentToken = localStorage.getItem("token");
+      if (this.state.isAuthenticated !== (currentToken !== null)) {
+        this.setState({ isAuthenticated: currentToken !== null });
+      }
+    }, 500); 
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  logoff = () => {
+    localStorage.removeItem("token");
+    this.setState({ isAuthenticated: false });
+  };
+
   render() {
     return (
       <>
         <div>
-            <nav className="navbar navbar-expand-xl navbar-dark bg-dark" aria-label="Third navbar example">
-              <div className="container-fluid">
-                <div className="collapse navbar-collapse" id="navbarsExample03">
-                  <ul className="navbar-nav me-auto mb-2 mb-sm-0">
-                    <li className="nav-item">
-                      <NavLink to={"/"} className="nav-link active link-info" aria-current="page">Home</NavLink>
-                    </li>
-                    <li className="nav-item">
-                      <NavLink to={"/charlas"} className="nav-link active link-info" aria-current="page">Charlas</NavLink>
-                    </li>
-                    <li className="nav-item">
-                      <NavLink to={"/create/charla"} className="nav-link active link-info" aria-current="page">Create Charla</NavLink>
-                    </li>
+          <nav className={`navbar navbar-expand-xl navbar-dark ${styles.navbar}`} aria-label="Third navbar example">
+            <div className="container-fluid">
+              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample03" aria-controls="navbarsExample03" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarsExample03">
+                <ul className={`navbar-nav me-auto mb-2 mb-sm-0 ${styles.navbarLeft}`}>
+                  <li className="nav-item">
+                    <NavLink to={"/"} className={`nav-link ${styles.navLink}`} aria-current="page">
+                      <FaHome className={styles.navbarIcon} /> Home
+                    </NavLink>
+                  </li>
+                </ul>
 
-                    {/* {localStorage.getItem("token")  == "" ? <div> */}
-                      <li>
-                      <NavLink
-                        to="/login"
-                        className="nav-link"
-                          state={{ isActive: true}}
-                      >
-                        Register
-                      </NavLink>
+                {/* Navbar a la derecha */}
+                <ul className={`navbar-nav mb-2 mb-sm-0 ${styles.navbarRight}`}>
+                  {!this.state.isAuthenticated && (
+                    <>
+                      <li className="nav-item">
+                        <NavLink to="/login" className={`nav-link ${styles.navLink}`} state={{ isActive: true }}>
+                          Register
+                        </NavLink>
+                      </li>
+                      <li className="nav-item">
+                        <NavLink to={"/login"} className={`nav-link ${styles.navLink}`} state={{ isActive: false }}>
+                          Login
+                        </NavLink>
+                      </li>
+                    </>
+                  )}
 
-                    </li>
-                    <li>
-                      <NavLink to={"/login"} className={"nav-link"} state={{isActive: false}} >Login</NavLink>
-                    </li>
-                    {/* </div> : console.log("Logged in")} */}
-                    
-                  </ul>
-                </div>
+                  {this.state.isAuthenticated && (
+                    <>
+                      <li className="nav-item">
+                        <NavLink to={"/charlas"} className={`nav-link ${styles.navLink}`} aria-current="page">
+                          Charlas
+                        </NavLink>
+                      </li>
+                      <li className="nav-item">
+                        <NavLink to={"/create/charla"} className={`nav-link ${styles.navLink}`} aria-current="page">
+                          Create Charla
+                        </NavLink>
+                      </li>
+                      <li className="nav-item">
+                        <NavLink to={"/login"} className={`nav-link ${styles.navLink} ${styles.navLinkLogout}`} aria-current="page" onClick={this.logoff}>
+                          Cerrar Sesion
+                        </NavLink>
+                      </li>
+                    </>
+                  )}
+                </ul>
               </div>
-            </nav>
+            </div>
+          </nav>
         </div>
+
       </>
-    )
+    );
   }
 }
