b04c4549ae122a8bf9dc9fdb7b9109fcb8058d34
valentintic <valentinpreuteseitic@gmail.com>
2025-01-13 11:02:53 +0100

    Arreglos Diseño Charlas y Rondas

--- src/services/Axios.js
+++ src/services/Axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosApi = axios.create({
-    baseURL: "https://apicharlasalumnostajamar.azurewebsites.net/", // Tu URL base
+    baseURL: "https://apicharlasalumnostajamar2025.azurewebsites.net/", // Tu URL base
 });
 
 axiosApi.interceptors.request.use(config => {
--- src/services/CharlasApi.js
+++ src/services/CharlasApi.js
@@ -3,6 +3,7 @@ import axiosApi from "./Axios";
 export const getCharlas = async () => {
     try {
         const response = await axiosApi.get("api/charlas/charlascurso/");
+        console.log(response.data[0].imagenCharla);
         return response.data;
     } catch (error) {
         console.log("Error getting Charlas", error);
