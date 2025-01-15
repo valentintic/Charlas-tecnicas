d259f6092550ddfd02177508381144510b658353
DoryanAlexandro <doryanamt@gmail.com>
2025-01-15 14:02:48 +0100

    servicios admin

--- src/services/UsuariosAdminService.js
+++ src/services/UsuariosAdminService.js
@@ -1,6 +1,6 @@
 import axiosApi from "./Axios"
 
-export const getProfesoresAsync = async () => {
+export const getProfesores = async () => {
     try {
         const response = await axiosApi.get("/api/Admin/Profesores")
         return response.data
@@ -9,11 +9,38 @@ export const getProfesoresAsync = async () => {
     }
 }
 
-export const getUsuariosActivosAsync = async () => {
+export const getUsuariosActivos = async () => {
     try {
         const response = await axiosApi.get("/api/Admin/UsuariosActivos")
         return response.data
     } catch (error) {
         console.log(error)
+    }
+}
+
+export const updateEstadoProfesores = async (usuario, estado ) => {
+    try {
+        const response = await axiosApi.put("/api/Admin/UpdateEstadoProfesor/"+usuario+"/true/"+ estado);
+        return response.data;
+    } catch (error) {
+        console.warn("Error updating alumno", error);
+    }
+}
+
+export const UpdateCursoUsuario = async (usuario, estado ) => {
+    try {
+        const response = await axiosApi.put("/api/Admin/UpdateCursoUsuario/"+usuario+"/true/"+ estado);
+        return response.data;
+    } catch (error) {
+        console.warn("Error updating alumno", error);
+    }
+}
+
+export const UpdateRoleUsuario = async (usuario, estado ) => {
+    try {
+        const response = await axiosApi.put("/api/Admin/UpdateRoleUsuario/"+usuario+"/true/"+ estado);
+        return response.data;
+    } catch (error) {
+        console.warn("Error updating alumno", error);
     }
 }
